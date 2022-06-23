2af84f2f7eac7f13a2905455e7fa06ddcc845461
Aldi <aldi.app@qubisa.com>
2022-06-24 03:18:31 +0700

    [main] fixing mysql password

--- internal/database/mysql.go
+++ internal/database/mysql.go
@@ -6,7 +6,7 @@ import (
 )
 
 func NewDatabaseConn() *gorm.DB {
-	dsn := "root:root@tcp(127.0.0.1:60082)/course2?parseTime=True&loc=Local"
+	dsn := "root:Aldi_wi2210@tcp(127.0.0.1:3306)/course2?parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("can't connect to database")
